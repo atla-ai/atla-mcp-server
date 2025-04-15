23d9900d2794133212cc3008704cec5f37189c1a
jake golden <jake@atla-ai.com>
2025-04-15 12:47:12 +0100

    simple example

--- src/atla_mcp_server/__main__.py
+++ src/atla_mcp_server/__main__.py
@@ -1,6 +1,12 @@
 """Entrypoint for the Atla MCP Server."""
 
-from atla_mcp_server import mcp
+from atla_mcp_server.server import mcp
+
+
+def main():
+    """Run the Atla MCP Server."""
+    mcp.run(transport="stdio")
+
 
 if __name__ == "__main__":
-    mcp.run(transport="stdio")
+    main()
