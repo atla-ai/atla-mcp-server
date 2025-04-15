804380c991f9b0a5d0619d369a17412d560b7a52
jake golden <jake@atla-ai.com>
2025-04-15 15:06:20 +0100

    minimal implementation from example

---
+++ src/remote/app.py
@@ -0,0 +1,18 @@
+"""Starlette app for remote hosting of the Atla MCP Server."""
+
+import uvicorn
+from starlette.applications import Starlette
+from starlette.routing import Mount
+
+from src.atla_mcp_server.server import mcp
+
+# https://github.com/modelcontextprotocol/python-sdk?tab=readme-ov-file#mounting-to-an-existing-asgi-server
+
+app = Starlette(
+    routes=[
+        Mount("/", app=mcp.sse_app()),
+    ]
+)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
